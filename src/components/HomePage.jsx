d7b47c8ff194f494933f145d70698c070ea01d01
selimsahnoun <sahnounselim@gmail.com>
2020-10-30 19:09:51 +0100

    picture render on the homepage component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -14,23 +14,13 @@ class HomePage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			//data: [], //list of houses from DB
-			/* 		view: 'HomePage',
-			chosenAddress: {},
-			pickupDate: 0,
-			returnDate: 0,
-			period: 0,
-      Price: 0, */
 			address: '',
 		};
-
 
 		this.handleEventchange = this.handleEventchange.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 	}
-	componentDidMount() {
-		//	this.props.fetchHouses();
-	}
+
 	handleClick() {
 		this.props.filterHouseByAddress(this.state.address);
 	}
@@ -38,24 +28,12 @@ class HomePage extends React.Component {
 		this.setState({
 			address: e.target.value,
 		});
-
-		/* 
-    var address = e.target.value;
-		var currentAdress = this.state.data.filter(
-			(house) => house.address === address
-		);
-		this.setState({
-			chosenAddress: currentAdress, //list of filtered addresses from DB
-		});
-		console.log(currentAdress);
- */
 	}
 
 	render() {
 		return (
-			<>
+			<div id="HomePage">
 				<h3 className="Title">Welcome to Cozy hysas!</h3>
-
 				<MDBContainer>
 					<MDBInput
 						label="Address: Where are you going? "
@@ -71,7 +49,7 @@ class HomePage extends React.Component {
 					</button>
 					{/*<DatePicker selected={startdate} onChange={date => setStartDate(date)} /> const [startDate, setStartDate] = useState(new Date());*/}
 				</MDBContainer>
-			</>
+			</div>
 		);
 	}
 }
