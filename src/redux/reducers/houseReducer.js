dc2db01040dd611431b4d0f657e2936657d7b4b4
selimsahnoun <sahnounselim@gmail.com>
2020-10-30 11:58:35 +0100

    before editing the list of houses comp

--- src/redux/reducers/houseReducer.js
+++ src/redux/reducers/houseReducer.js
@@ -1,11 +1,25 @@
-import { ADD_HOUSE } from '../actions/types';
+import { ADD_HOUSE, FILTERED_HOUSES } from '../actions/types';
 
-const initialState = {};
+const initialState = {
+	house: {
+		title: '',
+		imageUrl: '',
+		description: '',
+		adress: '',
+		price: 0,
+		startDate: null,
+		endDate: null,
+	},
+	listOfHouses: [],
+};
 
 const houseReducer = function (state = initialState, action) {
 	switch (action.type) {
 		case ADD_HOUSE: {
-			return action.payload;
+			return { ...state, house: action.payload };
+		}
+		case FILTERED_HOUSES: {
+			return { ...state, listOfHouses: action.payload };
 		}
 		default: {
 			return state;
